18e89ecc224a9963da1bac8bddb898860bfc4d36
EvelynB <evelyn.bautista@le-campus-numerique.fr>
2025-04-10 11:55:52 +0100

    Remed flask examp

---
+++ src/hello.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask, render_template  
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/gallery")
+def gallery():
+    return render_template('Gallery.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
